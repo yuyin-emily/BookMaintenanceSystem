be840cdfb37068819c832d6d6c8b2fc2e6531ddc
yuyin-emily <emilychen20030716@gmail.com>
2024-05-11 20:49:00 +0800

    template

--- BookMaintenanceSystem/urls.py
+++ BookMaintenanceSystem/urls.py
@@ -1,20 +1,7 @@
-"""BookMaintenanceSystem URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 import book.views as bviews
 import account.views as aviews
@@ -24,10 +11,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     
     path("", bviews.search),
-    path("create/", bviews.create),
-    path("edit/", bviews.edit),
-    path("details/", bviews.details),
-    path("landRecord/", bviews.landRecord),
+    # path("create/", bviews.create),
+    # path("edit/", bviews.edit),
+    # path("details/", bviews.details),
+    # path("lend_record/", bviews.lend_record),
     
     path("login/", aviews.sign_in),
     path("log_out/", aviews.log_out),
--- book/views.py
+++ book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from book.models import BookCategory,BookCode,BookData,BookLendRecord
+
 # Create your views here.
+def search(request):
+    try:
+        books = BookData.objects.all().order_by("id")
+    except:
+        errormessage = "讀取錯誤"
+        
+    return render(request,"book/search.html",locals())
