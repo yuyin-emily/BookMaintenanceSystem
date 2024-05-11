2ab76a24ee13152389ce8bb9900a4687d76a6d83
yuyin-emily <emilychen20030716@gmail.com>
2024-05-11 19:45:57 +0800

    loaded

--- book/admin.py
+++ book/admin.py
@@ -1,31 +1,23 @@
 from django.contrib import admin
 
 from book.models import BookCategory,BookCode,BookData,BookLendRecord
+
 # Register your models here.
 class BookCategoryAdmin(admin.ModelAdmin):
-    list_display = ("category_id","category_name")
-    list_filter = ("category_id","category_name")
-    search_fields = ("category_name",)
-    ordering = ("category_id",)
-    
+    list_display = ['category_id', 'category_name']
+
 class BookCodeAdmin(admin.ModelAdmin):
-    list_display = ("code_id","code_name")
-    list_filter = ("code_id","code_name")
-    search_fields = ("code_id",)
-    ordering = ("code_id",)
-    
+    list_display = ['code_id', 'code_name']
+
 class BookDataAdmin(admin.ModelAdmin):
-    list_display = ("name","category","author","publisher","publish_date","summary","price","keeper_id","status")
-    list_filter = ("name","category")
-    search_fields = ("name",)
-    ordering = ("name",)
+    list_display = ['id', 'name', 'category', 'author', 'publisher', 'publish_date', 'price', 'keeper_id', 'status']
+    list_filter = ['category', 'status']
+    search_fields = ['name', 'author']
 
 class BookLendRecordAdmin(admin.ModelAdmin):
-    list_display = ("book","borrower","borrow_date")
-    list_filter = ("book","borrower","borrow_date")
-    search_fields = ("book",)
-    ordering = ("book",)
-
+    list_display = ['id', 'book', 'borrower', 'borrow_date']
+    list_filter = ['borrow_date']
+    
 admin.site.register(BookCategory,BookCategoryAdmin)
 admin.site.register(BookCode,BookCodeAdmin)
 admin.site.register(BookData,BookDataAdmin)
--- book/models.py
+++ book/models.py
@@ -5,25 +5,27 @@ from account.models import Student
 # Create your models here.
 
 class BookCategory(models.Model):
-    category_id = models.CharField(max_length=10,null=False)
-    category_name = models.CharField(max_length=100,null=False)
-    
+    category_id = models.CharField(max_length=10, primary_key=True)
+    category_name = models.CharField(max_length=100)
+
 class BookCode(models.Model):
-    code_id = models.CharField(max_length=1,null=False)
-    code_name = models.CharField(max_length=100,null=False)
-    
+    code_id = models.CharField(max_length=1, primary_key=True)
+    code_name = models.CharField(max_length=100)
+
 class BookData(models.Model):
-    name = models.CharField(max_length=100,null=False)
-    category_id = models.ForeignKey(BookCategory,on_delete=models.CASCADE)
-    author = models.CharField(max_length=100,null=True)
-    publisher = models.CharField(max_length=100,null=True)
-    publish_date = models.DateField(null=True)
-    summary = models.CharField(max_length=40,null=True)
-    price = models.IntegerField(null=False)
-    keeper_id = models.IntegerField(null=False)
-    status = models.ForeignKey(BookCode,on_delete=models.CASCADE,max_length=24,null=False)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(BookCategory, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100, null=True, blank=True)
+    publisher = models.CharField(max_length=100, null=True, blank=True)
+    publish_date = models.DateField(null=True, blank=True)
+    summary = models.CharField(max_length=255, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    keeper_id = models.IntegerField(null=True, blank=True)
+    status = models.ForeignKey(BookCode, on_delete=models.CASCADE)
     
 class BookLendRecord(models.Model):
-    book = models.ForeignKey(BookData,on_delete=models.CASCADE,null=False)
-    borrower = models.ForeignKey(Student,on_delete=models.CASCADE,null=False)
-    borrow_date = models.DateField(null=True)
+    id = models.AutoField(primary_key=True)
+    book = models.ForeignKey(BookData, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Student, on_delete=models.CASCADE)
+    borrow_date = models.DateField()
