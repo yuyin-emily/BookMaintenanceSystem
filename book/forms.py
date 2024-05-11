aeb5698113dced18aa91694cc568fdde43ffe72d
yuyin-emily <emilychen20030716@gmail.com>
2024-05-12 02:03:07 +0800

    edit

--- book/forms.py
+++ book/forms.py
@@ -34,4 +34,29 @@ class BookDataForm(forms.ModelForm):
 
     def clean_publish_date(self):
         publish_date = self.cleaned_data['publish_date']
+        # if publish_date > timezone.now().date():
+        #     raise ValidationError("出版日期不得超過今日")
         return publish_date
+    
+class BookDataSearchForm(forms.ModelForm):
+
+    # category = forms.ModelChoiceField(queryset=BookCategory.objects.all(), widget=forms.Select(attrs={"class": "form-control"}))
+    
+    class Meta:
+        model = BookData
+        fields = ['name', 'category', 'keeper_id', 'status']
+        
+        widgets = {
+            'name': forms.TextInput(attrs={"class": "form-control"}),
+            'category': forms.Select(attrs={"class": "form-control"}),
+            'keeper_id': forms.Select(attrs={"class": "form-control"}),
+            'status': forms.Select(attrs={"class": "form-control"}),
+        }
+        labels = {
+            'name': '書名',
+            'category': '書籍類別',
+            'keeper_id': '借閱人',
+            'status': '借閱狀態',
+        }
+        
+        
--- book/views.py
+++ book/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, get_object_or_404,HttpResponseRedirect
 from book.models import BookCategory,BookCode,BookData,BookLendRecord
 from account.models import Student
 
-from book.forms import BookDataForm
+from book.forms import BookDataForm,BookDataSearchForm
 
 # Create your views here.
 def search(request):
     try:
         books = BookData.objects.all().order_by("id")
+        form = BookDataSearchForm()
         for book in books:
             category = BookCategory.objects.get(category_id=book.category_id)
             book.category_name = category.category_name
@@ -25,7 +26,8 @@ def search(request):
     return render(request,"book/search.html",locals())
 
 def detail(request, pk=None):
-    book = get_object_or_404(BookData, pk=pk)
+    book = get_object_or_404(BookData, id=pk)
+    form = BookDataForm(instance=book)
     return render(request, 'book/bookdata.html', locals())
 
 def create(request):
@@ -44,7 +46,7 @@ def edit(request, pk=None):
         form = BookDataForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("")
+            return HttpResponseRedirect("book/detail/"+str(pk)+"/")
     else:
         form = BookDataForm(instance=book)
     return render(request, "book/bookdata.html", locals())
